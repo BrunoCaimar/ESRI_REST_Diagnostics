2c920ac7ab1405460ac56e1742158611742b9646
Bruno Caimar <bruno.caimar@gmail.com>
2016-04-07 11:08:03 -0300

    Change to use bookmarklets.json from a file

--- gulpfile.js
+++ gulpfile.js
@@ -4,75 +4,38 @@ var bookmarklet = require('gulp-bookmarklet');
 var netscape = require('netscape-bookmarks');
 var through = require('through2');
 var File = require('vinyl');
+var bookmarklets_config = require('./bookmarklets.json');
 
 gulp.task('bookmarklet', ['clean-min'], function() {
     return gulp.src(['**/*.js', '!./**/*.min.js', '!gulpfile.js', '!node_modules/**/*.*'])
         .pipe(bookmarklet({format: 'js'}))
         .pipe(gulp.dest('.'));
 });
+
 var esri_rest_diagnostics = function() {
-	// TODO: Read it from file (bookmarks.json)
-	var bookmarks = {
-	"Map Folder Level:": {
-		"contents": {
-			"Spatial Reference Comparison": "SpatialRefCompare.min.js",
-			"MapService Data Extractor v 0.3": "MapServiceData.min.js",
-			"Map Viewer v. 0.5": "MapViewer.min.js"
-		}
-	},
-	"Map Service Level:": {
-		"contents": {
-			"Layer Property Extractor v. 0.1": "MapLayerData.min.js",
-			"Layer Feature Counter v. 0.1": "MapLayerCount.min.js",
-			"Tiled Service Tester v. 0.1": "TileTester.min.js",
-			"Map Viewer v. 0.5": "MapViewer.min.js"
-		}
-	},
-	"Map Layer Level:": {
-		"contents": {
-			"Field Value Tester (null test only) v. 0.4": "FieldTester.min.js",
-			"Populated Field Value Tester (null or empty string) v. 0.9": "FieldTester_NullOrEmpty.min.js",
-			"Domain Data Counter v. 0.3": "DomainDataCounter.min.js"
-		}
-	},
-	"Query Page Level:": {
-		"contents": {
-			"Select By Attributes Query Helper v. 0.3": "QueryHelper.min.js",
-			"Give me all Query Helper": "GiveMeAll_QueryHelper.min.js",
-			"Count them Up Query Helper": "CountThemUp_QueryHelper.min.js",
-			"Everything But Geometry Query Helper": "AllNoGeo_QueryHelper.min.js",
-			"Select Distinct Query Helper": "SelectDistinct_QueryHelper.min.js"
-		}
-	},
-	"Miscellaneous:": {
-		"contents": {
-			"Rest Search v 0.5": "RestSearch.min.js",
-			"Print Task Made Easy BETA": "PrintTaskMadeEasy.min.js",
-			"Geometry Helper": "GeometryHelper.min.js"
-		}
-	}
-};
-	 return through.obj(function(file, encoding, callback) {
-		function doSomethingWithTheFile(file) {
-			var file_name = file.path.split("\\").pop();
-			for (var section in bookmarks) {
-			   for (var bookmark in bookmarks[section].contents) {
-				   var bookmark_file_name = bookmarks[section].contents[bookmark];
+	return through.obj(function(file, encoding, callback) {
+		function generate_bookmarklets_html(file) {
+			var file_delimiter = file.path.toString().indexOf("\\") > -1 ? "\\" : "/";
+			var file_name = file.path.split(file_delimiter).pop();
+			// TO DO: Think in a better way to do it
+			for (var section in bookmarklets_config) {
+			   for (var bookmark in bookmarklets_config[section].contents) {
+				   var bookmark_file_name = bookmarklets_config[section].contents[bookmark];
 				   if (bookmark_file_name.toLowerCase() === file_name.toLowerCase())
 				   {
-					   bookmarks[section].contents[bookmark] = file.contents.toString();
+					   bookmarklets_config[section].contents[bookmark] = file.contents.toString();
 					   break;
 				   }
 				}
 			}
 		};
-		callback(null, doSomethingWithTheFile(file));
-  }, function(callback) {
-		var html = netscape(bookmarks);
-		// TODO: Move it to another function
-		html = html.replace("Bookmarks Menu", "ESRI Rest Diagnostics");
-		html = html.replace("Bookmarks", "ESRI Rest Diagnostics");
-		html = html.replace(/<\/H1>/, "</H1><p>This page provides links to tools that can be used to inspect and pull out data out of ArcGIS REST Services.</p>")
+		callback(null, generate_bookmarklets_html(file));
+	}, function(callback) {
+		var html = netscape(bookmarklets_config);
+		
+		html = html.replace(/Bookmarks Menu/gi, "ESRI Rest Diagnostics");
+		html = html.replace(/Bookmarks/gi, "ESRI Rest Diagnostics");
+		html = html.replace(/<\/H1>/gi, "</H1><p>This page provides links to tools that can be used to inspect and pull out data out of ArcGIS REST Services.</p>")
 		html = html.replace(/<DL>/gi, "<UL>");
 		html = html.replace(/<\/DL>/gi, "</UL>");
 		html = html.replace(/<DT>/gi, "<LI>");
@@ -83,15 +46,15 @@ var esri_rest_diagnostics = function() {
 		html = html.replace(/<\/UL><p>/gi, "</UL>");
 		html += "</body></html>"
 
-		var bookmarklet_file = new File({		
+		var bookmarklet_file = new File({
 		  // TODO: Get name as a parameter
 		  path: "bookmarklets.html",
 		  contents: new Buffer(html)
 		});
 
-        this.push(bookmarklet_file);
-	  callback(null);
-  });    
+		this.push(bookmarklet_file);
+		callback(null);
+	});
 };
 
 gulp.task('esri_rest_diagnostics', ['bookmarklet'], function () {
